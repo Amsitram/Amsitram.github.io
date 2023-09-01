96213aa326d3c710f6e9556ddfdaec77268373bb
Martin Guillemet <intelligence@Precieux.local>
2023-09-02 00:38:57 +0200

    Revert "font fix"
    This reverts commit dc8142d3ead78981e3e8ef9a944afdad71b99f68.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Martin Guillemet`,
-    siteTitleAlt: `Martin Guillemet · Portfolio`,
-    siteHeadline: `Martin Guillemet · Compositing & Motion Design`,
-    siteUrl: `http://martin.guillemet.org`,
-    siteDescription: `Martin Guillemet travaille actuellement sur son projet de diplôme à l'école Estienne (2022)`,
-    siteLanguage: `fr`,
-    siteImage: `/banner.png`,
-    author: `Martin Guillemet`,
+    siteTitle: `Cara`,
+    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteUrl: `https://cara.lekoarts.de`,
+    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@lekoarts_de`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Martin Guillemet · Portfolio`,
-        short_name: `Martin Guillemet`,
-        description: `Martin Guillemet travaille actuellement sur son projet de diplôme à l'école Estienne (2022)`,
+        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        short_name: `Cara`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
@@ -36,8 +36,14 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/favicon.svg`,
-            type: `image/svg+xml`,
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
           },
         ],
       },
